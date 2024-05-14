511b803a1cd1cdc3faf5415fb9785cb7517b7d54
Maulana Ibrahim <maulputra09@gmail.com>
2024-05-14 13:49:59 +0700

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import { NavBar } from "@/components/navbar";
 import { HOST } from "@/constants/api";
@@ -52,6 +53,7 @@ export default function RootLayout({
       <body className={inter.className}>
       <NavBar />
         {children}
+        <Analytics/>
       </body>
     </html>
   );
